60c1fe3c074f7c6355dc07f92977d03850c7919c
Your Name <kevin.kds80@gmail.com>
2023-01-07 12:37:19 -0300

    add toJson and parseJson instances

--- src/Schemas/Responses/Task.hs
+++ src/Schemas/Responses/Task.hs
@@ -12,15 +12,23 @@
 
 module Schemas.Responses.Task where
 
-import Data.Text as T
-import GHC.Generics
+import Data.Text as T ( pack, Text )
+import GHC.Generics ( Generic )
 
-import Mu.Quasi.GRpc
-import Mu.Schema
+import Mu.Quasi.GRpc ( grpc )
+import Mu.Schema ( FromSchema, ToSchema )
+
+import Data.Aeson
+    ( (.:),
+      withObject,
+      object,
+      FromJSON(parseJSON),
+      KeyValue((.=)),
+      ToJSON(toJSON) )
 
 grpc "TodoSchema" id "todolist.proto"
 
-data Task = Task { 
+data Task = Task {
     storyId :: T.Text,
     taskId :: T.Text,
     description :: T.Text,
@@ -30,12 +38,38 @@ data Task = Task {
              , ToSchema   TodoSchema "Task"
              , FromSchema TodoSchema "Task" )
 
-data Tasks = Tasks { tasks :: [Task] } 
+instance ToJSON Task where
+    toJSON Task {
+                storyId = storyId,
+                taskId = taskId,
+                description = description,
+                finished = finished
+        } = object [
+                T.pack "_id" .= taskId
+                , T.pack "storyId" .= storyId
+                , T.pack "description" .= description
+                , T.pack "finished" .= finished
+            ]
+
+instance FromJSON Task where
+    parseJSON = withObject "Task" $ \obj -> do
+            storyId <- obj .: T.pack "storyId"
+            taskId <- obj .: T.pack "_id"
+            description <- obj .: T.pack "description"
+            finished <- obj .: T.pack "finished"
+            return (Task {
+                storyId = storyId,
+                taskId = taskId,
+                description = description,
+                finished = finished
+            })
+
+data Tasks = Tasks { tasks :: [Task] }
     deriving ( Eq, Show, Generic
             , ToSchema   TodoSchema "Tasks"
             , FromSchema TodoSchema "Tasks" )
 
-data DeleteResult = DeleteResult { success :: Bool } 
+data DeleteResult = DeleteResult { success :: Bool }
     deriving ( Eq, Show, Generic
             , ToSchema   TodoSchema "DeleteResult"
             , FromSchema TodoSchema "DeleteResult" )
