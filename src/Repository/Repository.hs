f066104168a9f25619760dcc549d0e82be1f95bd
Your Name <kevin.kds80@gmail.com>
2023-01-07 13:17:45 -0300

    adjust find operations

--- src/Repository/Repository.hs
+++ src/Repository/Repository.hs
@@ -5,7 +5,7 @@
 
 module Repository.Repository where
 
-import Data.Text
+import Data.Text (Text, unpack)
 import Database.MongoDB
 import Data.AesonBson
 import Data.Aeson (Object, FromJSON, ToJSON, decode, encode)
@@ -14,9 +14,24 @@ import Repository.Connection (Connection)
 
 import Schemas.Responses.Task as R
 
-findOneTask query = do
-    findOne 
-        (select query "todolist")
+findTasks (pipe, dbName) storyId page limit = do
+    results <- access pipe master dbName (
+        rest =<< find
+            (select ["storyId" =: unpack storyId] "todolist")
+            {sort = [], limit = limit, skip = page}
+        )
+    let obj = map (aesonify)  results
+    let (Just tasks) = (decode . encode) obj :: (Maybe [R.Task])
+    return tasks
+
+findOneTask (pipe, dbName) query = do
+    (Just insertedResult) <- access pipe master dbName
+        (findOne 
+            (select query "todolist"))
+    let obj = aesonify $ insertedResult
+    let (Just task) = (decode . encode) obj :: (Maybe R.Task)
+    return task
+    
 
 insertTask :: Connection -> Text -> Text -> IO R.Task
 insertTask (pipe, dbName) storyId description = do
@@ -29,11 +44,7 @@ insertTask (pipe, dbName) storyId description = do
     insertedId <- access pipe master dbName
         (insert "todolist" insertionData)
 
-    (Just insertedResult) <- access pipe master dbName
-        (findOneTask ["_id" =: insertedId])
+    insertedResult <- findOneTask (pipe, dbName) ["_id" =: insertedId]
 
-    let obj = aesonify $ insertedResult
-    let (Just task) = (decode . encode) obj :: (Maybe R.Task)
-    print task
-    return task
+    return insertedResult
     
